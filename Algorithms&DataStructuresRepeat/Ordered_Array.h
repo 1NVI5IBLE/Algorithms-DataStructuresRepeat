a1607c8264cc4c83a596f82c2c4834d7ebb15c3b
1NVI5IBLE <62433063+1NVI5IBLE@users.noreply.github.com>
2025-07-21 10:22:42 +0100

    Added two constructors. One with a default grow size and one that lets you choose the grow size.

--- Algorithms&DataStructuresRepeat/Ordered_Array.h
+++ Algorithms&DataStructuresRepeat/Ordered_Array.h
@@ -7,5 +7,17 @@ private:
 	int size;
 	int capacity;
 	int growSize;
+
+public:
+
+	OrderedArray() : size(0), capacity(10), growSize(10) {
+		elements = new T[capacity];
+	}
+
+	OrderedArray(int grow_size) : size(0), capacity(grow_size), growSize(grow_size) {
+		elements = new T[capacity]
+	}
+
+
 };
 
